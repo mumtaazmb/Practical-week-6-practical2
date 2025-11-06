cb7e330c2e6609bb811133ce7d04aa7b89d7cdac
mumtaazmb <m.mahir.bare@gmail.com>
2025-11-06 12:35:43 +0000

    Update sketch.js

--- exercise1-1/sketch.js
+++ exercise1-1/sketch.js
@@ -1,99 +1,38 @@
+
+class Star {
+    constructor(x,y){
+        this.x = x
+        this.y = y
+        
+    }
+    draw(){
+    fill(255, 234, 0);
+    noStroke();
+    triangle(this.x, this.y - 50, this.x - 20, this.y, this.x + 20, this.y);
+    triangle(this.x - 50, this.y - 20, this.x, this.y - 20, this.x, this.y + 10);
+    triangle(this.x + 50, this.y - 20, this.x, this.y - 20, this.x, this.y + 10);
+    triangle(this.x - 20, this.y - 5, this.x, this.y + 10, this.x - 35, this.y + 30);
+    triangle(this.x, this.y + 10, this.x + 20, this.y - 5, this.x + 35, this.y + 30);
+    }
+    fall(speed){
+        this.y = this.y + speed
+    }
+
+    shoot(speed){
+        this.x = this.x + speed
+    }
+}
+
+let fallingStar = new Star (200,0)
+let shootingStar = new Star (0,200)
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
     background(0);
+    shootingStar.shoot(5)
+    shootingStar.draw();
+    fallingStar.fall(2);
+    fallingStar.draw()
 }
-
-function star(x, y) {
-    fill(255, 234, 0);
-    noStroke();
-    triangle(x, y - 50, x - 20, y, x + 20, y);
-    triangle(x - 50, y - 20, x, y - 20, x, y + 10);
-    triangle(x + 50, y - 20, x, y - 20, x, y + 10);
-    triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
-    triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Star {
-//     constructor(x, y) {
-//         this.x = x;
-//         this.y = y;
-//     }
-
-//     draw() {
-//         star(this.x, this.y);
-//     }
-
-//     fall(speed) {
-//         this.y = this.y + speed;
-//     }
-
-//     shoot(speed) {
-//         this.x = this.x + speed;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
